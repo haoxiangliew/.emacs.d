c39cd303dcde74baf5b58b329d62e51df0be2149
Hao Xiang Liew <haoxiangliew@gmail.com>
2023-04-23 15:34:59 -0400

    Update config

--- init.el
+++ init.el
@@ -739,12 +739,12 @@
   :after calfw)
 
 ;; elcord
-;; (use-package elcord
-;;   :init
-;;   (elcord-mode)
-;;   :config
-;;   (setq elcord-use-major-mode-as-main-icon t
-;; 	elcord--editor-name (concat "Emacs " emacs-version)))
+(use-package elcord
+  :init
+  (elcord-mode)
+  :config
+  (setq elcord-use-major-mode-as-main-icon t
+	elcord--editor-name (concat "Emacs " emacs-version)))
 
 ;; notmuch
 (use-package notmuch
@@ -859,14 +859,112 @@
 		 :files ("dist" "*.el"))
   :init
   (setq copilot-node-executable (replace-regexp-in-string "[()]" "" (format "%s" (file-expand-wildcards "/nix/store/*-nodejs-16*/bin/node"))))
-  :config
+  (defvar copilot-disabled-modes '(shell-mode
+				   inferior-python-mode
+				   eshell-mode
+				   term-mode
+				   vterm-mode
+				   comint-mode
+				   compilation-mode
+				   debugger-mode
+				   dired-mode-hook
+				   compilation-mode-hook
+				   flutter-mode-hook
+				   minibuffer-mode-hook)
+    "Modes in which copilot is inconvenient.")
+  (defvar copilot-manual-mode nil
+    "When `t' will only show completions when manually triggered, e.g. via M-C-<return>.")
+  (defvar copilot-enable-for-org nil
+    "Should copilot be enabled for org-mode buffers?")
   (defun copilot-tab ()
-    "Copilot completion for tab"
+    "Copilot completion for tab, else corfu-complete, else indent"
     (interactive)
     (or (copilot-accept-completion)
+	(corfu-complete)
 	(indent-for-tab-command)))
+  (defun copilot-complete-or-accept ()
+    "Accept completion if it exists"
+    (interactive)
+    (if (copilot--overlay-visible)
+	(progn
+	  (copilot-accept-completion)
+	  (open-line 1)
+	  (next-line))
+      (copilot-complete)))
+  (defun copilot-quit ()
+    "Clear the overlay"
+    (interactive)
+    (condition-case err
+	(when copilot--overlay
+          (lexical-let ((pre-copilot-disable-predicates copilot-disable-predicates))
+		       (setq copilot-disable-predicates (list (lambda () t)))
+		       (copilot-clear-overlay)
+		       (run-with-idle-timer 1.0 nil
+					    (lambda ()
+					      (setq copilot-disable-predicates pre-copilot-disable-predicates)))))
+      (error handler)))
+  (defun copilot-complete-if-active (next-func n)
+    (let ((completed (when copilot-mode (copilot-accept-completion))))
+      (unless completed (funcall next-func n))))
+  (defun copilot-disable ()
+    "Helper for `copilot-disabled-modes'."
+    (copilot-mode -1))
+  (defun copilot-enable-predicate ()
+    "Show completions"
+    (and
+     (eq (get-buffer-window) (selected-window))))
+  (defun copilot-disable-predicate ()
+    "Disable completions."
+    (or copilot-manual-mode
+	(member major-mode copilot-disabled-modes)
+	(and (not copilot-enable-for-org) (eq major-mode 'org-mode))))
+  (defun copilot-change-activation ()
+    "Switch between three activation modes:
+     - automatic (default): copilot will automatically overlay completions
+     - manual: you need to press a key (M-C-<return>) to trigger completions
+     - off: copilot is completely disabled"
+    (interactive)
+    (if (and copilot-mode copilot-manual-mode)
+	(progn
+          (message "deactivating copilot")
+          (global-copilot-mode -1)
+          (setq copilot-manual-mode nil))
+      (if copilot-mode
+          (progn
+            (message "activating copilot in manual mode")
+            (setq copilot-manual-mode t))
+	(message "activating copilot")
+	(global-copilot-mode))))
+  (defun copilot-toggle-for-org ()
+    "Toggle copilot activation in org mode"
+    (interactive)
+    (setq copilot-enable-for-org (not copilot-enable-for-org))
+    (message "copilot for org is %s" (if copilot-enable-for-org "enabled" "disabled")))
+  :config
   (with-eval-after-load 'copilot
-    (define-key copilot-mode-map (kbd "C-c <tab>") #'copilot-tab)))
+    ;; keybindings that are active when copilot shows completions
+    (define-key copilot-mode-map (kbd "M-C-<next>") #'copilot-next-completion)
+    (define-key copilot-mode-map (kbd "M-C-<prior>") #'copilot-previous-completion)
+    (define-key copilot-mode-map (kbd "M-C-<right>") #'copilot-accept-completion-by-word)
+    (define-key copilot-mode-map (kbd "M-C-<down>") #'copilot-accept-completion-by-line)
+
+    ;; global keybindings
+    (define-key global-map (kbd "M-C-<return>") #'copilot-complete-or-accept)
+    (define-key global-map (kbd "M-C-<escape>") #'copilot-change-activation)
+
+    ;; Do copilot-quit when pressing C-g
+    (advice-add 'keyboard-quit :before #'copilot-quit)
+
+    ;; complete by pressing right or tab but only when copilot completions are
+    ;; shown. This means we leave the normal functionality intact
+    (advice-add 'right-char :around #'copilot-complete-if-active)
+    (advice-add 'indent-for-tab-command :around #'copilot-complete-if-active)
+
+    ;; deactivate copilot for certain modes
+    (add-to-list 'copilot-enable-predicates #'copilot-enable-predicate)
+    (add-to-list 'copilot-disable-predicates #'copilot-disable-predicate)
+
+    (global-copilot-mode)))
 
 ;; flymake
 ;; check https://www.emacswiki.org/emacs/FlyMake#h5o-2
