7d07eeef58299800e6ecba8c161956659dcc7aa9
Hao Xiang Liew <haoxiangliew@gmail.com>
2024-03-18 14:26:52 -0400

    update config

--- init.el
+++ init.el
@@ -117,7 +117,7 @@
   (when (memq window-system '(mac ns x))
     (exec-path-from-shell-initialize))
   :config
-  (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "CC" "CXX" "LANG" "LC_CTYPE" "LDFLAGS" "NIX_SSL_CERT_FILE" "NIX_PATH"))
+  (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "CC" "CXX" "LANG" "LC_CTYPE" "LDFLAGS" "NIX_SSL_CERT_FILE" "NIX_PATH" "LIBRARY_PATH"))
     (add-to-list 'exec-path-from-shell-variables var)))
 
 ;; emacs config
