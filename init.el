fa8af92cfe7e88991beef21b40c4116acd176927
Hao Xiang Liew <haoxiangliew@gmail.com>
2024-03-18 14:38:44 -0400

    update config

--- init.el
+++ init.el
@@ -113,12 +113,11 @@
 
 ;; load-path
 (use-package exec-path-from-shell
-  :init
-  (when (memq window-system '(mac ns x))
-    (exec-path-from-shell-initialize))
   :config
   (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "CC" "CXX" "LANG" "LC_CTYPE" "LDFLAGS" "NIX_SSL_CERT_FILE" "NIX_PATH" "LIBRARY_PATH"))
-    (add-to-list 'exec-path-from-shell-variables var)))
+    (add-to-list 'exec-path-from-shell-variables var))
+  (when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize)))
 
 ;; emacs config
 (use-package emacs
