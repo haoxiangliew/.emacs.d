abd576d3afc91f9d14faabf673d462906ed55d86
Hao Xiang Liew <haoxiangliew@gmail.com>
2024-04-12 11:48:15 -0400

    update config

--- init.el
+++ init.el
@@ -633,7 +633,6 @@
   :hook ((css-mode html-mode sass-mode scss-mode web-mode) . rainbow-mode))
 
 ;; magit
-(use-package transient)
 (use-package magit
   :bind
   ("C-x g" . magit-status)
@@ -974,31 +973,31 @@ changes, which means that `git-gutter' needs to be re-run.")
   (flyspell-lazy-mode +1))
 
 ;; copilot
-;; (use-package copilot
-;;   :ensure (copilot :repo "https://github.com/copilot-emacs/copilot.el"
-;; 		   :files ("dist" "*.el"))
-;;   :hook ((prog-mode . copilot-turn-on-unless-buffer-read-only)
-;; 	 (emacs-lisp-mode . (lambda ()
-;; 			      (setq-local copilot--indent-warning-printed-p t))))
-;;   :bind (("C-c h" . copilot-mode)
-;; 	 (:map copilot-completion-map
-;; 	       ("C-g" . 'copilot-clear-overlay)
-;; 	       ("<tab>" . 'copilot-tab)
-;; 	       ("TAB" . 'copilot-tab)))
-;;   :init
-;;   (defun copilot-turn-on-unless-buffer-read-only ()
-;;     "Turn on `copilot-mode' if the buffer is writable."
-;;     (unless (or buffer-read-only (not (buffer-file-name (current-buffer))))
-;;       (copilot-mode 1)))
-;;   :config
-;;   (setq copilot-indent-offset-warning-disable t)
-;;   (defun copilot-tab ()
-;;     "Copilot completion for tab"
-;;     (interactive)
-;;     (if (copilot--overlay-visible)
-;; 	(progn
-;; 	  (copilot-accept-completion))
-;;       (copilot-complete))))
+(use-package copilot
+  :ensure (copilot :repo "https://github.com/copilot-emacs/copilot.el"
+		   :files ("dist" "*.el"))
+  :hook ((prog-mode . copilot-turn-on-unless-buffer-read-only)
+	 (emacs-lisp-mode . (lambda ()
+			      (setq-local copilot--indent-warning-printed-p t))))
+  :bind (("C-c h" . copilot-mode)
+	 (:map copilot-completion-map
+	       ("C-g" . 'copilot-clear-overlay)
+	       ("<tab>" . 'copilot-tab)
+	       ("TAB" . 'copilot-tab)))
+  :init
+  (defun copilot-turn-on-unless-buffer-read-only ()
+    "Turn on `copilot-mode' if the buffer is writable."
+    (unless (or buffer-read-only (not (buffer-file-name (current-buffer))))
+      (copilot-mode 1)))
+  :config
+  (setq copilot-indent-offset-warning-disable t)
+  (defun copilot-tab ()
+    "Copilot completion for tab"
+    (interactive)
+    (if (copilot--overlay-visible)
+	(progn
+	  (copilot-accept-completion))
+      (copilot-complete))))
 
 ;; envrc
 (use-package envrc
